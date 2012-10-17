10fb0fd36e13b9835e685a708b2b7618e7176afa
Tobias Crawley <tcrawley@gmail.com>
2012-10-16 23:08:10 -0400

    Try doing it the right way.

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :profiles {:ci {:plugins [[lein-immutant "0.12.0"]]}})
--- setup_profiles.sh
+++
@@ -1,2 +0,0 @@
-ls ~/.lein/
-echo '{:user {:plugins [lein-immutant "0.12.0"]}}' > ~/.lein/profiles.clj
